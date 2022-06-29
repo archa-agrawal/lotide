69efcaa9e22c5272fe60edb51ccc730caa7f20ca
Archana Agrawal <archana.agrawal3@outlook.com>
2022-06-29 16:18:56 -0400

    count letters not spaces

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  let passEmoji = String.fromCodePoint(9989) + String.fromCodePoint(9989) + String.fromCodePoint(9989);
+  let failEmoji = String.fromCodePoint(10060) + String.fromCodePoint(10060) + String.fromCodePoint(10060);
+  if (actual === expected) {
+    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (string) => {
+  const letterCount = {};
+
+  for (let letter of string) {
+    if (!!letterCount[letter]) {
+      letterCount[letter]++;
+    } else if (letter !== ' ') {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+}
+console.log(countLetters("lighthouse in the house"))
