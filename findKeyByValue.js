1e80121c2a6cc859ada24569dfc4c6677f542245
Archana Agrawal <archana.agrawal3@outlook.com>
2022-06-29 17:34:47 -0400

    created and tested

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  let passEmoji = String.fromCodePoint(9989) + String.fromCodePoint(9989) + String.fromCodePoint(9989);
+  let failEmoji = String.fromCodePoint(10060) + String.fromCodePoint(10060) + String.fromCodePoint(10060);
+  if (actual === expected) {
+    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, value) => {
+
+  for (let element in obj) {
+    if (obj[element] === value) {
+      return element;
+    }
+  }
+  return undefined
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+const contacts = {
+  riya: 98796976,
+  Mira: 76868797,
+  Lila: 78768768
+}
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(contacts, 87686575), undefined)
+assertEqual(findKeyByValue(contacts, 78768768), 'Lila')
+
