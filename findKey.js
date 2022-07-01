a70a1dbff2c6f49f5108b57868127079f8c58290
Archana Agrawal <archana.agrawal3@outlook.com>
2022-06-30 22:38:51 -0400

    find key function

---
+++ findKey.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const assertEqual = function(actual, expected) {
+  let passEmoji = String.fromCodePoint(9989) + String.fromCodePoint(9989) + String.fromCodePoint(9989);
+  let failEmoji = String.fromCodePoint(10060) + String.fromCodePoint(10060) + String.fromCodePoint(10060);
+  if (actual === expected) {
+    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (obj, callback) => {
+  for (const [key, value] of Object.entries(obj)) {
+    if (callback(value)) {
+      return key;
+    }
+  } 
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 5 }
+}, x => x.stars === 2), 'noma') 
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 5 }
+}, x => x.stars === 5), 'Akelarre') 
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 5 }
+}, x => x.stars === 6), undefined) 
