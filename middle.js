fb44a9d9a14f45c3a7534a2791c38c0623bf1ab4
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-04 15:05:36 -0400

    refactored using modules and separated test code

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = (arr1, arr2) => {
-  let passEmoji = String.fromCodePoint(9989) + String.fromCodePoint(9989) + String.fromCodePoint(9989);
-  let failEmoji = String.fromCodePoint(10060) + String.fromCodePoint(10060) + String.fromCodePoint(10060);
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`${passEmoji} Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-}
-
 const middle = (arr) => {
   const middleArr = [];
 
@@ -35,9 +13,5 @@ const middle = (arr) => {
   }
   return middleArr;
 }
-assertArraysEqual(middle([1]),[]) // => []
-assertArraysEqual(middle([1, 2]), []) 
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+
+module.exports = middle;
