0c70a38331ba161480467280e1631478b4553fca
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-04 16:00:28 -0400

    refactored using mocha $ chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,21 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual')
 
-assertArraysEqual(middle([1]),[]) // => []
-assertArraysEqual(middle([1, 2]), []) 
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+
+describe('middle', () => {
+  it('should return empty array when array has only 1 value', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it('should return empty array when array has only 2 values', () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  it('should return middle number when array has more then 2 values', () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it('should return middle value when array length is an odd number and more than 3', () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+  it('should return 2 middle values when array length is even number', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,23 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ['hall', 'room', 'door', 'window'];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 4);
+// const words = ['hall', 'room', 'door', 'window'];
+// console.log(tail(words)); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 4);
+
+describe('tail', () => {
+  it('should return empty array if an empty array is passed', () => {
+    assert.deepEqual(tail([]), [])
+  });
+  it('should return empty array if input array has only 1 value', () => {
+    assert.deepEqual(tail([1]), [])
+  });
+  it('should return an array without its 0th value', () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3,4])
+  });
+  it('should not mutate the input array', () => {
+    const inputArray = [1,2,3,4];
+    tail(inputArray)
+    assert.deepEqual(inputArray, [1,2,3,4])
+  });
+});
