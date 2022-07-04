08c0854ce7f760b3e8007eca4f6052bc657f8066
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-04 14:48:13 -0400

    refactored using module and separated test code

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let passEmoji = String.fromCodePoint(9989) + String.fromCodePoint(9989) + String.fromCodePoint(9989);
-  let failEmoji = String.fromCodePoint(10060) + String.fromCodePoint(10060) + String.fromCodePoint(10060);
-  if (actual === expected) {
-    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
@@ -19,11 +9,4 @@ const eqArrays = (arr1, arr2) => {
   }
   return true;
 }
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-
+module.exports = eqArrays;
